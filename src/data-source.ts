f7da80d142a68c9cb3efe76c1f469b09423184bd
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2023-08-29 23:02:03 +0100

    Edited testing code and entity foreign key relations to support onDelete: SET NULL

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,14 +7,15 @@ import { Comment } from "./entitys/comment.entity"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host:  "localhost",// RISE.HOST,
-    port: 5432,
-    username: "postgres",
-    password: "risevest", 
-    database: "postgres",
+    host: RISE.HOST,
+    port: parseInt(RISE.PORT),
+    username: RISE.DATABASE_NAME,
+    password: RISE.DATABASE_PASSWORD, 
+    database: RISE.DATABASE,
     synchronize: true,
     logging: false,
     entities: [User, Post, Comment],
     migrations: [],
     subscribers: [],
+    ssl: true, // Add this line to enable SSL/TLS
 })
--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express"
 
 export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-    console.log(err)
     return res.status(err.statusCode  || 500).send(err.message);
 }
