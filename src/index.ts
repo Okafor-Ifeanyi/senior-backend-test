78e34111474902168576f345a974f969e6b75af9
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2023-09-04 10:07:47 +0100

    fix: postgresSQL debugging

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { AppDataSource } from "./data-source"
 import app from './app'
 import { RISE } from "./config/constants.config"
 
-AppDataSource.initialize().then(async () => {
+// AppDataSource.initialize().then(async () => {
     app.listen(RISE.EXPRESS_PORT)
     console.log(`Express server has started on port ${RISE.EXPRESS_PORT}`)
 
-}).catch(error => console.log(error))
+// }).catch(error => console.log(error))
+
+// console.log("I am active")
